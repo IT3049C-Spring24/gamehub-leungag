55913663d8193cafdabade5712d63ed67293e35f
leungag <leungag@mail.uc.edu>
2024-04-02 12:55:21 -0400

    edited the link in the react link

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,15 +4,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
+
 const Navigation = () => {
   return (
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/rps-react">Home</Link>
         </li>
         <li>
-          <Link to="./App.jsx">RPS</Link>
+          <Link to="/App">RPS</Link>
         </li>
         <li>
           <Link to="/TicTacToe">TicTacToe</Link>
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './routes/HomePage.jsx'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <HomePage />
+      <Routes> 
+        <Route path="/rps-react" element={<HomePage/>}/>
+        <Route path='/App' element={<App/>}/>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
