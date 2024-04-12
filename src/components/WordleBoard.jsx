04acfca4527b163644a62f06e39c7034e173da94
leungag <leungag@mail.uc.edu>
2024-04-11 22:49:47 -0400

    Have a function that fetch random words

--- src/components/WordleBoard.jsx
+++ src/components/WordleBoard.jsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react"
+
 
 function WordleBoard(){
+    const [solution, setSolution] = useState(null)
+
+
     return(
         <>
-            <h1>bord</h1>
+            <h1>bord</h1> 
+            {solution && <div>Solution is {setSolution}</div>}
         </>
     )
 }
+
+
+async function getRandomWord(){
+    useState (() => {
+        fetch('https://it3049c-hangman.fly.dev/')
+            .then(res=> res.json())
+            .then(json => {
+                console.log(json)
+                const wordFetch = json
+                setSolution(wordFetch.word)
+            })
+    })
+}
+
+
 
 export default WordleBoard
