a07a31b23ecbf2d7e43d2ad85d5487ade87c07ae
leungag <leungag@mail.uc.edu>
2024-04-15 14:39:24 -0400

    Output the current guess on row of square

--- src/components/RowWordle.jsx
+++ src/components/RowWordle.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import '../assets/Wordle.css'
 // making the square by cycling through the 6 array
-export default function Row( {guess}) {
+export default function Row( {guess, currentGuess}) {
     
     if(guess){ // take each letter in the guess to output in a square. No value then give empty row
         return (
             <div className="row past">
                 {guess.map((l,i) => (
                     <div key={i} className={l.color}>{l.key}</div>
+                ))}
+            </div>
+        )
+    }
+
+    if(currentGuess){
+        const letters = currentGuess.split('')
+        return (
+            <div className="row current">
+                {letters.map((letter, index) =>(
+                    <div key={index} className="filled">{letter}</div>
+                ))}
+                {[...Array(5 - letters.length)].map((value, index) => ( /*Keep sqaure rows in place */
+                    <div key={index}></div>
                 ))}
             </div>
         )
--- src/components/WordleGrid.jsx
+++ src/components/WordleGrid.jsx
@@ -5,6 +5,9 @@ export default function Grid({currentGuess,guesses,turn}) {
     return(
         <div>
             {guesses.map((guess, index)=> {
+                if (turn === index){ /* Output current guess on a row matching the turn*/
+                    return <Row key={index} currentGuess={currentGuess}/>
+                }
                 return <Row key={index} guess={guess}/>
             })}
         </div>
